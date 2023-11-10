5b202a0091aa2798be6797d750cff51c79e083ac
Morteza-Khedri <mkhedry3123@gmail.com>
2023-11-10 23:40:55 +0330

    added swap test and fork test foundry

---
+++ test/foundry/Swap.t.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.7;
+
+import "forge-std/Test.sol";
+import "../../contracts/token/IndexToken.sol";
+import "../../contracts/Swap.sol";
+
+contract CounterTest is Test {
+
+
+    uint256 internal constant SCALAR = 1e20;
+
+    IndexToken public indexToken;
+    Swap public swap;
+
+    address feeReceiver = vm.addr(1);
+    address newFeeReceiver = vm.addr(2);
+    address minter = vm.addr(3);
+    address newMinter = vm.addr(4);
+    address methodologist = vm.addr(5);
+
+
+    event FeeReceiverSet(address indexed feeReceiver);
+    event FeeRateSet(uint256 indexed feeRatePerDayScaled);
+    event MethodologistSet(address indexed methodologist);
+    event MethodologySet(string methodology);
+    event MinterSet(address indexed minter);
+    event SupplyCeilingSet(uint256 supplyCeiling);
+    event MintFeeToReceiver(address feeReceiver, uint256 timestamp, uint256 totalSupply, uint256 amount);
+    event ToggledRestricted(address indexed account, bool isRestricted);
+
+
+    function setUp() public {
+        indexToken = new IndexToken();
+        indexToken.initialize(
+            "Anti Inflation",
+            "ANFI",
+            1e18,
+            feeReceiver,
+            1000000e18
+        );
+        indexToken.setMinter(minter);
+
+        //swap
+        swap = new Swap();
+    }
+
+    function testInitialized() public {
+        // counter.increment();
+        assertEq(indexToken.owner(), address(this));
+        assertEq(indexToken.feeRatePerDayScaled(), 1e18);
+        assertEq(indexToken.feeTimestamp(), block.timestamp);
+        assertEq(indexToken.feeReceiver(), feeReceiver);
+        assertEq(indexToken.methodology(), "");
+        assertEq(indexToken.supplyCeiling(), 1000000e18);
+        assertEq(indexToken.minter(), minter);
+    }
+
+    function testMintOnlyMinter() public {
+        vm.expectRevert("IndexToken: caller is not the minter");
+        indexToken.mint(address(this), 1000e18);
+        assertEq(indexToken.balanceOf(address(this)), 0);
+    }
+
+    
+
+    
+}
