3be5df5837262548076a0c71758635088abfc7d5
soheilprs <soheilparsaei19@protonmail.com>
2024-12-14 23:37:31 -0800

    Added more tests

--- test/foundry/ChainlinkClient.t.sol
+++ test/foundry/ChainlinkClient.t.sol
@@ -13,6 +13,8 @@ contract ChainlinkClientTest is Test, ChainlinkClient {
     MockApiOracle private mockOracle;
     LinkTokenInterface private linkToken;
 
+    mapping(bytes32 => address) internal s_pendingRequests;
+
     function setUp() public {
         linkToken = LinkTokenInterface(address(new LinkToken()));
         mockOracle = new MockApiOracle(address(linkToken));
@@ -63,13 +65,11 @@ contract ChainlinkClientTest is Test, ChainlinkClient {
         // assertTrue(s_pendingRequests[requestId] != address(0));
     }
 
-    
-
-    
-
     function testGetNextRequestCount() public {
         uint256 initialCount = getNextRequestCount();
-        sendChainlinkRequest(buildChainlinkRequest("specId", address(this), this.fulfill.selector), 1 * LINK_DIVISIBILITY);
+        sendChainlinkRequest(
+            buildChainlinkRequest("specId", address(this), this.fulfill.selector), 1 * LINK_DIVISIBILITY
+        );
         uint256 newCount = getNextRequestCount();
 
         assertEq(newCount, initialCount + 1);
@@ -77,5 +77,60 @@ contract ChainlinkClientTest is Test, ChainlinkClient {
 
     function fulfill(bytes32 requestId, bytes32 data) public recordChainlinkFulfillment(requestId) {
         // Fulfillment logic
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+
+    function testAddChainlinkExternalRequest() public {
+        bytes32 requestId = keccak256("externalRequest");
+        address externalOracle = address(mockOracle);
+
+        addChainlinkExternalRequest(externalOracle, requestId);
+
+        assertEq(getPendingRequest(requestId), externalOracle);
+    }
+
+    function testCancelChainlinkRequest() public {
+        bytes32 requestId = sendChainlinkRequest(
+            buildChainlinkRequest("specId", address(this), this.fulfill.selector), 1 * LINK_DIVISIBILITY
+        );
+
+        uint256 expiration = block.timestamp + 1 days;
+        vm.warp(block.timestamp + 2 days);
+
+        cancelChainlinkRequest(requestId, 1 * LINK_DIVISIBILITY, this.fulfill.selector, expiration);
+
+        assertEq(getPendingRequest(requestId), address(0));
+    }
+
+    function testPendingRequestsMapping() public {
+        bytes32 requestId = sendChainlinkRequest(
+            buildChainlinkRequest("specId", address(this), this.fulfill.selector), 1 * LINK_DIVISIBILITY
+        );
+
+        uint256 expiration = block.timestamp + 1 days;
+        vm.warp(block.timestamp + 2 days);
+
+        cancelChainlinkRequest(requestId, 1 * LINK_DIVISIBILITY, this.fulfill.selector, expiration);
+
+        assertEq(getPendingRequest(requestId), address(0));
+    }
+
+    function testFailSendChainlinkRequestInvalidPayment() public {
+        bytes32 specId = "specId";
+        Chainlink.Request memory req = buildChainlinkRequest(specId, address(this), this.fulfill.selector);
+
+        vm.expectRevert("unable to transferAndCall to oracle");
+        sendChainlinkRequest(req, 0);
+    }
+
+    function testUnauthorizedFulfillment() public {
+        bytes32 requestId = sendChainlinkRequest(
+            buildChainlinkRequest("specId", address(this), this.fulfill.selector), 1 * LINK_DIVISIBILITY
+        );
+
+        vm.prank(address(0xdeadbeef));
+        vm.expectRevert("Source must be the oracle of the request");
+        fulfill(requestId, keccak256("data"));
     }
 }
