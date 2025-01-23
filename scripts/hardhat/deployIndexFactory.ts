3697c7cbfcdcaf0c164c135e1fd8dc9267e1aa7f
Morteza-Khedri <mkhedry3123@gmail.com>
2025-01-23 14:27:34 +0330

    added hardhat comminted

---
+++ scripts/hardhat/deployIndexFactory.ts
@@ -0,0 +1,38 @@
+import { ethers, upgrades } from "hardhat";
+import { goerliAnfiIndexTokenAddress, goerliEthUsdPriceFeed, goerliExternalJobIdBytes32, goerliFactoryV2Address, goerliFactoryV3Address, goerliLinkAddress, goerliOracleAdress, goerliQouterAddress, goerliRouterV2Address, goerliRouterV3Address, goerliWethAddress, seploliaWethAddress, sepoliaAnfiIndexTokenAddress, sepoliaARBEIIndexTokenAddress, sepoliaEthUsdPriceFeed, sepoliaFactoryV3Address, sepoliaLinkAddress, sepoliaRouterV3Address, sepoliaSCIIndexTokenAddress } from "../contractAddresses";
+// const { ethers, upgrades, network, hre } = require('hardhat');
+
+async function deployIndexToken() {
+  
+  const [deployer] = await ethers.getSigners();
+
+  const IndexFactory = await ethers.getContractFactory("IndexFactory");
+  console.log('Deploying IndexFactory...');
+
+  const indexToken = await upgrades.deployProxy(IndexFactory, [
+      sepoliaARBEIIndexTokenAddress, //token
+      sepoliaLinkAddress, //link token address
+      goerliOracleAdress, //oracle address
+      goerliExternalJobIdBytes32, // jobId
+      sepoliaEthUsdPriceFeed, // price feed
+      seploliaWethAddress, //weth address
+      goerliQouterAddress, // qoute
+      sepoliaRouterV3Address, // routerv3
+      sepoliaFactoryV3Address, //factoryv3
+      goerliRouterV2Address, //routerv2
+      goerliFactoryV2Address //factoryv2
+  ], { initializer: 'initialize' });
+
+  await indexToken.waitForDeployment()
+
+  console.log(
+    `IndexFactory deployed: ${ await indexToken.getAddress()}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+deployIndexToken().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
